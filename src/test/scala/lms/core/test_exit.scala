e156942027016f4a7cbfd9da312ed7c51a4756ef
Gregory Essertel <gregory.essertel@gmail.com>
2019-04-14 13:02:46 -0400

    Add test for Rep[Unit] vs Unit bug

---
+++ src/test/scala/lms/core/test_exit.scala
@@ -0,0 +1,68 @@
+package lms
+package core
+
+import lms.core.stub._
+import lms.core.virtualize
+import lms.macros.SourceContext
+
+class ExitTest extends TutorialFunSuite {
+  val under = "core/"
+
+  test("exit_rep1") {
+    val driver = new DslDriverC[Int,Unit] {
+
+      def repExit(x: Rep[Int]): Rep[Unit] = Wrap[Unit](Adapter.g.reflect("exit", Unwrap(x)))
+      @virtualize
+      def myassert(cond: Rep[Boolean], msg: String): Rep[Unit] = if (cond) { printf(msg); repExit(0) }
+
+      @virtualize
+      def snippet(arg: Rep[Int]) = {
+        myassert(arg > 0, "fail")
+      }
+    }
+    check("exit_rep1", driver.code, "c")
+  }
+
+  test("exit_rep2") {
+    val driver = new DslDriverC[Int,Unit] {
+
+      def repExit(x: Rep[Int]): Rep[Unit] = Wrap[Unit](Adapter.g.reflect("exit", Unwrap(x)))
+      @virtualize
+      def myassert(cond: Rep[Boolean], msg: String): Unit = if (cond) { printf(msg); repExit(0) }
+
+      @virtualize
+      def snippet(arg: Rep[Int]) = {
+        myassert(arg > 0, "fail")
+      }
+    }
+    check("exit_rep2", driver.code, "c")
+  }
+
+  test("exit1") {
+    val driver = new DslDriverC[Int,Unit] {
+
+      @virtualize
+      def myassert(cond: Rep[Boolean], msg: String): Rep[Unit] = if (cond) { printf(msg); exit(0) }
+
+      @virtualize
+      def snippet(arg: Rep[Int]) = {
+        myassert(arg > 0, "fail")
+      }
+    }
+    check("exit1", driver.code, "c")
+  }
+
+  test("exit2") {
+    val driver = new DslDriverC[Int,Unit] {
+
+      @virtualize
+      def myassert(cond: Rep[Boolean], msg: String): Unit = if (cond) { printf(msg); exit(0) }
+
+      @virtualize
+      def snippet(arg: Rep[Int]) = {
+        myassert(arg > 0, "fail")
+      }
+    }
+    check("exit2", driver.code, "c")
+  }
+}
