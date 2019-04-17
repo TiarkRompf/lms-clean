2b00a307c960530b31b53bb1ec508825256ce615
Gregory Essertel <gregory.essertel@gmail.com>
2019-04-17 16:32:20 -0400

    test for if DCE

---
+++ src/out/core/if_dead_effect_value.check.c
@@ -0,0 +1,55 @@
+#include <fcntl.h>
+#include <errno.h>
+#include <err.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#ifndef MAP_FILE
+#define MAP_FILE MAP_SHARED
+#endif
+int fsize(int fd) {
+  struct stat stat;
+  int res = fstat(fd,&stat);
+  return stat.st_size;
+}
+int printll(char* s) {
+  while (*s != '\n' && *s != ',' && *s != '\t') {
+    putchar(*s++);
+  }
+  return 0;
+}
+long hash(char *str0, int len)
+{
+  unsigned char* str = (unsigned char*)str0;
+  unsigned long hash = 5381;
+  int c;
+  while ((c = *str++) && len--)
+  hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+  return hash;
+}
+void Snippet(char*);
+int main(int argc, char *argv[])
+{
+  if (argc != 2) {
+    printf("usage: query <filename>\n");
+    return 0;
+  }
+  Snippet(argv[1]);
+  return 0;
+}
+/*****************************************
+Emitting C Generated Code
+*******************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+void Snippet(int x0) {
+  if (x0 == 0) printf("hkjfd\n"); else printf("fsgl");
+  printf("hello");
+}
+/*****************************************
+End of C Generated Code
+*******************************************/
---
+++ src/test/scala/lms/core/test_if_dce.scala
@@ -0,0 +1,81 @@
+package lms
+package core
+
+import lms.core.stub._
+import lms.core.virtualize
+import lms.macros.SourceContext
+
+class IfDCETest extends TutorialFunSuite {
+  val under = "core/"
+
+  test("if_dead_unchecked") {
+    val driver = new DslDriverC[Int,Unit] {
+      @virtualize
+      def snippet(arg: Rep[Int]) = {
+        if (arg == 0) { uncheckedPure[Int]("foo") } else  { unchecked[Int]("bar") }
+        printf("hello")
+      }
+    }
+    check("if_dead_unchecked", driver.code, "c")
+  }
+
+  test("if_dead_pure") {
+    val driver = new DslDriverC[Int,Unit] {
+      @virtualize
+      def snippet(arg: Rep[Int]) = {
+        if (arg == 0) { 1 + arg } else  { 3 }
+        printf("hello")
+      }
+    }
+    check("if_dead_pure", driver.code, "c")
+  }
+  test("if_dead_effect") {
+    val driver = new DslDriverC[Int,Unit] {
+      @virtualize
+      def snippet(arg: Rep[Int]) = {
+        if (arg == 0) { printf("hkjfd\n") } else  { printf("fsgl") }
+      }
+    }
+    check("if_dead_effect", driver.code, "c")
+  }
+  test("if_dead_effect_value") {
+    val driver = new DslDriverC[Int,Unit] {
+      @virtualize
+      def snippet(arg: Rep[Int]) = {
+        if (arg == 0) { printf("hkjfd\n"); 3 } else  { printf("fsgl"); arg + 6 }
+        printf("hello")
+      }
+    }
+    check("if_dead_effect_value", driver.code, "c")
+  }
+  test("if_unchecked") {
+    val driver = new DslDriverC[Int,Unit] {
+      @virtualize
+      def snippet(arg: Rep[Int]) = {
+        val x = if (arg == 0) { uncheckedPure[Int]("foo") } else  { unchecked[Int]("bar") }
+        printf(">> %d\n", x)
+      }
+    }
+    check("if_unchecked", driver.code, "c")
+  }
+  test("if_pure") {
+    val driver = new DslDriverC[Int,Unit] {
+      @virtualize
+      def snippet(arg: Rep[Int]) = {
+        val x = if (arg == 0) { 1 + arg } else  { 3 }
+        printf(">> %d\n", x)
+      }
+    }
+    check("if_pure", driver.code, "c")
+  }
+  test("if_effect") {
+    val driver = new DslDriverC[Int,Unit] {
+      @virtualize
+      def snippet(arg: Rep[Int]) = {
+        val x = if (arg == 0) { printf("hkjfd\n"); 3 } else  { printf("fsgl"); arg + 6 }
+        printf(">> %d\n", x)
+      }
+    }
+    check("if_effect", driver.code, "c")
+  }
+}
