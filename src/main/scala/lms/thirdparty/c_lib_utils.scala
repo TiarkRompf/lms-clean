0bc09e14d2bc42a8d7cbe3edab9a832e674ccee6
feiwang3311 <feiwang3311@gmail.com>
2020-06-27 20:06:14 -0400

    more fix

--- src/main/scala/lms/thirdparty/c_lib_utils.scala
+++ src/main/scala/lms/thirdparty/c_lib_utils.scala
@@ -52,6 +52,12 @@ trait CCodeGenLibStruct extends ExtendedCCodeGen {
       emit(s"${remap(m)} ")
       shallow(s)
       emitln(";")
+    // allow "lib-struct" to take parameters
+    case Node(s, "lib-struct", Const(m: Manifest[_])::args, _) =>
+      emit(s"${remap(m)} "); shallow(s);
+      emit("("); shallow(args.head);
+      args.tail.foreach(a => {emit(", "); shallow(a)})
+      emitln(");")
     case _ => super.traverse(n)
   }
 }
@@ -82,3 +88,32 @@ trait CCodeGenLibFunction extends ExtendedCCodeGen {
 
 trait CLibs extends Base with CMacro with LibStruct with LibFunction
 trait CCodeGenLibs extends CCodeGenCMacro with CCodeGenLibStruct with CCodeGenLibFunction
+
+trait CudaFunction extends Base {
+  def cudaFunction[T:Manifest](m: String, configs: Seq[lms.core.Backend.Exp],
+    rhs: lms.core.Backend.Exp*)(rkeys: Seq[Int], wkeys: Seq[Int], pkeys: Set[Int], keys: lms.core.Backend.Const*): Rep[T] = {
+    val readKeys = rkeys.map(rhs(_))
+    val writeKeys = wkeys.map(rhs(_)) ++ keys
+    val defs = Seq(lms.core.Backend.Const(m), lms.core.Backend.Const(configs.length), lms.core.Backend.Const(pkeys)) ++ configs ++ rhs
+    Wrap[T](Adapter.g.reflectEffect("lib-function", defs: _*)(readKeys: _*)(writeKeys: _*))
+  }
+}
+
+trait CudaCodeGenLibFunction extends ExtendedCCodeGen {
+  override def shallow(n: Node): Unit = n match {
+    case Node(s, "lib-function", Const(m:String)::Const(c:Int)::Const(pkeys:Set[Int])::defs, _) =>
+      val configs = defs.take(c)
+      val rhs = defs.drop(c)
+      val last = rhs.length - 1
+      emit(s"$m<<<"); shallow(configs.head);
+      configs.tail.foreach(a => {emit(", "); shallow(a)});
+      emit(">>>(");
+      rhs.zipWithIndex.foreach { case(r, index) =>
+        if (pkeys.contains(index)) emit("&")
+        shallow(r)
+        if (index < last) emit(", ")
+      }
+      emit(")")
+    case _ => super.shallow(n)
+  }
+}
