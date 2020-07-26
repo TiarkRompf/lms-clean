2f883c7cf32acdb20d288cdd9dfb9080842b7471
feiwang3311 <feiwang3311@gmail.com>
2020-07-26 18:34:17 -0400

    add stack_array

--- src/main/scala/lms/collection/ArrayOps.scala
+++ src/main/scala/lms/collection/ArrayOps.scala
@@ -64,3 +64,35 @@ trait ArrayOps { b: Base =>
 }
 
 // FIXME(feiw) CodeGen of Array is still not yet separated from ExtendedScalaCodeGen and ExtendedCCodeGen
+
+// StackArray is the array using stack (i.e. int a[5] = {1,2,3,4,5};)
+trait StackArrayOps extends ArrayOps { b: Base =>
+
+  def NewStackArray[T:Manifest](x: Rep[Int]): Rep[Array[T]] = {
+    Wrap[Array[T]](Adapter.g.reflectMutable("NewStackArray", Unwrap(x)))
+  }
+
+  def StackArray[T:Manifest](xs: Rep[T]*): Rep[Array[T]] = {
+    Wrap[Array[T]](Adapter.g.reflectMutable("StackArray", xs.map(Unwrap(_)): _*))
+  }
+}
+
+trait CCodeGenStackArray extends ExtendedCCodeGen {
+
+  override def traverse(n: Node): Unit = n match {
+    case n @ Node(s, "NewStackArray", List(x), _) =>
+      val tpe = remap(typeMap.get(s).map(_.typeArguments.head).getOrElse(manifest[Unknown]))
+      emit(s"$tpe "); shallow(s); emit("["); shallow(x); emitln("];")
+    case n @ Node(s, "StackArray", xs, _) =>
+      val tpe = remap(typeMap.get(s).map(_.typeArguments.head).getOrElse(manifest[Unknown]))
+      emit(s"$tpe "); shallow(s); emit("[] = {"); shallow(xs.head)
+      xs.tail.foreach(x => {emit(", "); shallow(x)}); emitln("};")
+    case _ => super.traverse(n)
+  }
+
+  override def mayInline(n: Node): Boolean = n match {
+    case Node(s, "NewStackArray", _, _) => false
+    case Node(s, "StackArray", _, _) => false
+    case _ => super.mayInline(n)
+  }
+}
