19258d4bd736401809afb000373525f286ed59b9
tan279 <tan279@purdue.edu>
2020-08-13 16:16:42 -0400

    rename and generaic path parameter

--- src/main/scala/lms/core/stub.scala
+++ src/main/scala/lms/core/stub.scala
@@ -1241,8 +1241,8 @@ abstract class DslDriverC[A: Manifest, B: Manifest] extends DslSnippet[A, B] wit
     out.close
     (new java.io.File(executable)).delete
     import scala.sys.process._
-    val includes = codegen.composePaths(codegen.includePaths, "-I")
-    val libraryPaths = codegen.composePaths(codegen.libraryPaths, "-L")
+    val includes = codegen.joinPaths(codegen.includePaths, "-I")
+    val libraryPaths = codegen.joinPaths(codegen.libraryPaths, "-L")
     val pb: ProcessBuilder = s"$compilerCommand $sourceFile -o $executable $libraries $includes $libraryPaths"
     time("gcc") { pb.lines.foreach(Console.println _) }
     (a: A) => (s"$executable $a": ProcessBuilder).lines.foreach(Console.println _)
