a8881731b292127e5c12794819ec9ba59356e10e
feiwang3311 <feiwang3311@gmail.com>
2020-09-11 13:54:43 -0400

    add the check file

---
+++ src/out/transformer/deviced_tensorshow.check.cu
@@ -0,0 +1,33 @@
+/*****************************************
+Emitting C Generated Code
+*******************************************/
+#include <string.h>
+#include <cblas.h>
+#include <stdlib.h>
+#include <cuda_header.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+/**************** Snippet ****************/
+void Snippet(int x0) {
+  float* x1 = (float*)malloc(0 * sizeof(float));
+  CUDA_CALL(cudaMalloc(&x1, (size_t)(6 * sizeof(int))));
+  float* x2 = (float*)malloc(6 * sizeof(float));
+  CUDA_CALL(cudaMemcpy(x2, x1, (size_t)(6 * sizeof(int)), cudaMemcpyDeviceToHost));
+  int x3 = 0;
+  while (x3 != 6) {
+    printf("%f ", x2[x3]);
+    x3 = x3 + 1;
+  }
+}
+/*****************************************
+End of C Generated Code
+*******************************************/
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("usage: %s <arg>\n", argv[0]);
+    return 0;
+  }
+  Snippet(atoi(argv[1]));
+  return 0;
+}
