0cfdbdc905495bb404e6f51142edad2d47b5323d
Luke Jiang <39086121+luke-jiang@users.noreply.github.com>
2020-11-23 11:55:24 -0500

    update output check files

--- src/out/thirdparty/nccl/p2p-scatter.check.cu
+++ src/out/thirdparty/nccl/p2p-scatter.check.cu
@@ -4,70 +4,79 @@ Emitting C Generated Code
 #include "nccl_header.h"
 #include <string.h>
 #include <stdlib.h>
-#include <cuda_header.h>
+#include "cuda_header.h"
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <mpi_header.h>
+#include "mpi_header.h"
 /**************** Snippet ****************/
 void Snippet(int x0) {
   int x1 = 0;
   int x2 = 0;
   MPICHECK(MPI_Init(NULL, NULL));
-  MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &x1));
-  int x3 = MPI_Comm_size(MPI_COMM_WORLD, &x2);
+  int x3 = MPI_Comm_rank(MPI_COMM_WORLD, &x1);
   MPICHECK(x3);
-  ncclUniqueId x4;
-  if (x1 == 0) NCCLCHECK(ncclGetUniqueId(&x4));
-  MPICHECK(MPI_Bcast(&x4, NCCL_UNIQUE_ID_BYTES, MPI_BYTE, 0, MPI_COMM_WORLD));
-  CUDA_CALL(cudaSetDevice(0));
-  cudaStream_t x5;
-  CudaError_T x6 = cudaStreamCreateWithFlags(&x5, cudaStreamDefault);
-  CUDA_CALL(x6);
-  ncclComm_t x7;
-  ncclResult_t x8 = ncclCommInitRank(&x7, x2, x4, x1);
-  NCCLCHECK(x8);
-  float* x9 = (float*)malloc(33554432 * sizeof(float));
-  int x10 = 0;
-  while (x10 != 33554432) {
-    x9[x10] = 1.0;
-    x10 = x10 + 1;
+  int x4 = MPI_Comm_size(MPI_COMM_WORLD, &x2);
+  MPICHECK(x4);
+  printf("myRank: %d, nRanks: %d\n", x1, x2);
+  ncclUniqueId x5;
+  ncclComm_t x6;
+  cudaStream_t x7;
+  if (x1 == 0) NCCLCHECK(ncclGetUniqueId(&x5));
+  MPICHECK(MPI_Bcast(&x5, NCCL_UNIQUE_ID_BYTES, MPI_BYTE, 0, MPI_COMM_WORLD));
+  float* x8 = (float*)malloc(2014 * sizeof(float));
+  int x9 = 0;
+  while (x9 != 2014) {
+    x8[x9] = 2.0;
+    x9 = x9 + 1;
   }
-  float** x11 = (float**)malloc(x2 * sizeof(float*));
-  int x12 = x2;
-  int x13 = 0;
-  while (x13 != x12) {
-    int x14 = x13;
-    CUDA_CALL(cudaMalloc(&x11[x14], 33554432));
-    CUDA_CALL(cudaMemcpy(x11[x14], x9, 33554432, cudaMemcpyHostToDevice));
-    x13 = x13 + 1;
+  CUDA_CALL(cudaSetDevice(x1));
+  float** x10 = (float**)malloc(x2 * sizeof(float*));
+  int x11 = x2;
+  int x12 = 0;
+  while (x12 != x11) {
+    int x13 = x12;
+    CUDA_CALL(cudaMalloc(&x10[x13], 2014));
+    CUDA_CALL(cudaMemcpy(x10[x13], x8, 2014, cudaMemcpyHostToDevice));
+    x12 = x12 + 1;
   }
-  float* x15 = (float*)malloc(0 * sizeof(float));
-  CUDA_CALL(cudaMalloc(&x15, (size_t)(33554432 * sizeof(float))));
+  float* x14 = (float*)malloc(0 * sizeof(float));
+  CUDA_CALL(cudaMalloc(&x14, (size_t)(2014 * sizeof(float))));
+  cudaError_t x15 = cudaStreamCreateWithFlags(&x7, cudaStreamDefault);
+  CUDA_CALL(x15);
+  ncclResult_t x16 = ncclCommInitRank(&x6, x2, x5, x1);
+  NCCLCHECK(x16);
   NCCLCHECK(ncclGroupStart());
   if (x1 == 0) {
-    int x16 = x2;
-    int x17 = 0;
-    while (x17 != x16) {
-      int x18 = x17;
-      NCCLCHECK(ncclSend(x11[x18], 33554432, ncclFloat, x18, x7, x5));
-      x17 = x17 + 1;
+    int x17 = x2;
+    int x18 = 0;
+    while (x18 != x17) {
+      int x19 = x18;
+      NCCLCHECK(ncclSend(x10[x19], 2014, ncclFloat, x19, x6, x7));
+      x18 = x18 + 1;
     }
-    NCCLCHECK(ncclRecv(x15, 33554432, ncclFloat, 0, x7, x5));
   }
+  NCCLCHECK(ncclRecv(x14, 2014, ncclFloat, 0, x6, x7));
   NCCLCHECK(ncclGroupEnd());
-  CUDA_CALL(cudaStreamSynchronize(x5));
-  CUDA_CALL(cudaMemcpy(x9, x15, 33554432, cudaMemcpyDeviceToHost));
-  int x19 = 0;
-  while (x19 != 33554432) {
-    if (x9[0] != 1) printf("error");
-    x19 = x19 + 1;
+  CUDA_CALL(cudaStreamSynchronize(x7));
+  CUDA_CALL(cudaMemcpy(x8, x14, 2014, cudaMemcpyDeviceToHost));
+  int x20 = 0;
+  int x21 = 0;
+  while (x21 != 2014) {
+    if (x8[0] != 2) x20 = x20 + 1;
+    x21 = x21 + 1;
   }
-  CUDA_CALL(cudaFree(x11));
-  CUDA_CALL(cudaFree(x15));
-  NCCLCHECK(ncclCommDestroy(x7));
+  int x22 = x2;
+  int x23 = 0;
+  while (x23 != x22) {
+    CUDA_CALL(cudaFree(x10[x23]));
+    x23 = x23 + 1;
+  }
+  CUDA_CALL(cudaFree(x14));
+  NCCLCHECK(ncclCommDestroy(x6));
   MPICHECK(MPI_Finalize());
-  printf("[MPI Rank %d] Success \n", x1);
+  if (x20 != 0) printf("[MPI Rank %d] Found %d errors.\n", x1, x20);
+  else printf("[MPI Rank %d] Success \n", x1);
 }
 /*****************************************
 End of C Generated Code
