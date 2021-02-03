11730ed2846f00e2df7b23a288ceef8faaf58b3a
Luke Jiang <39086121+luke-jiang@users.noreply.github.com>
2021-02-03 01:11:49 -0500

    change interface of ConvBackwardData and ConvBackwardFilter to include hyperparameters. This info can be used in the backward pass to fetch conv filter

--- src/main/scala/lms/transformation/distributed_tensor/distributedTensorConv.scala
+++ src/main/scala/lms/transformation/distributed_tensor/distributedTensorConv.scala
@@ -16,19 +16,23 @@ import Backend._
 trait FixedSizeDistributedTensorConvTypeLess extends FixedSizeDistributedTensorMutationTypeLess {
 
   def ConvBackwardData(weight: TENSOR, filter: TENSOR, doutput: TENSOR, alpha: Float, beta: Float, 
+                      padding: Seq[Int], strides: Seq[Int], dilation: Seq[Int],
                       anno: Anno, __pos: SourceContext): TENSOR = {
     assert(weight.et == filter.et && filter.et == doutput.et)
     val res_tt = weight.tensor_type
     (new TENSOR(Adapter.g.reflectRead("tensor_conv_bwd_data", C(res_tt), C(anno), 
-      filter.x, doutput.x, C(alpha), C(beta))(weight.x, filter.x, doutput.x)).withSrcType(__pos, weight.et))
+      filter.x, doutput.x, C(alpha), C(beta, C(padding), C(strides), C(dilation)))
+      (weight.x, filter.x, doutput.x)).withSrcType(__pos, weight.et))
   }
 
   def ConvBackwardFilter(weight: TENSOR, filter: TENSOR, doutput: TENSOR, alpha: Float, beta: Float, 
+                        padding: Seq[Int], strides: Seq[Int], dilation: Seq[Int],
                         anno: Anno, __pos: SourceContext): TENSOR = {
     assert(weight.et == filter.et && filter.et == doutput.et)
     val res_tt = filter.tensor_type
     (new TENSOR(Adapter.g.reflectRead("tensor_conv_bwd_filter", C(res_tt), C(anno), 
-      weight.x, doutput.x, C(alpha), C(beta))(weight.x, filter.x, doutput.x)).withSrcType(__pos, weight.et))
+      weight.x, doutput.x, C(alpha), C(beta), C(padding), C(strides), C(dilation))
+      (weight.x, filter.x, doutput.x)).withSrcType(__pos, weight.et))
   }
 
   override def aircopCollect(node: Node, forwardNodes: mutable.ArrayBuffer[Node],
@@ -46,11 +50,11 @@ trait FixedSizeDistributedTensorConvTypeLess extends FixedSizeDistributedTensorM
         val y = new TENSOR(transform(b))      // filter
 
         (() => {
-          val a_grad = ConvBackwardData(x, y, gradMap(s), alpha, beta, anno, pos)
+          val a_grad = ConvBackwardData(x, y, gradMap(s), alpha, beta, padding, strides, dilation, anno, pos)
           Accumulate(gradMap(a), a_grad, anno); ()
         }) +=: backwardNodes
         (() => {
-          val b_grad = ConvBackwardFilter(x, y, gradMap(s), alpha, beta, anno, pos)
+          val b_grad = ConvBackwardFilter(x, y, gradMap(s), alpha, beta, padding, strides, dilation, anno, pos)
           Accumulate(gradMap(b), b_grad, anno); ()
         }) +=: backwardNodes
 
