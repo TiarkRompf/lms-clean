971e124af903bf559572c68cc1ebbf2d1e559ee6
Luke Jiang <39086121+luke-jiang@users.noreply.github.com>
2021-02-25 21:22:08 -0500

    update

--- src/out/transformer/distributed_tensor/dropout.check.cu
+++ src/out/transformer/distributed_tensor/dropout.check.cu
@@ -20,35 +20,27 @@ __global__ void x12(float* x13, float x14, int x15) {
     x17 = x17 + x16;
   }
 }
-__global__ void x20(bool* x21, bool x22, int x23) {
+__global__ void x20(float* x21, float* x22, int x23) {
+  // begin generating kernel function for ACCUM of type Float
   int x24 = gridDim.x * blockDim.x;
   int x25 = threadIdx.x + blockIdx.x * blockDim.x;
   while (x25 < x23) {
-    x21[x25] = x22;
+    int x26 = x25;
+    x21[x26] = x21[x26] + x22[x26];
     x25 = x25 + x24;
-  }
-}
-__global__ void x26(float* x27, float* x28, int x29) {
-  // begin generating kernel function for ACCUM of type Float
-  int x30 = gridDim.x * blockDim.x;
-  int x31 = threadIdx.x + blockIdx.x * blockDim.x;
-  while (x31 < x29) {
-    int x32 = x31;
-    x27[x32] = x27[x32] + x28[x32];
-    x31 = x31 + x30;
   }
   // end generating kernel function for ACCUM of type Float
 }
-__global__ void x33(float* x34, float* x35, float* x36, int x37) {
+__global__ void x27(float* x28, float* x29, float* x30, int x31) {
   // begin generating kernel function for SGD of type Float
-  int x38 = gridDim.x * blockDim.x;
-  int x39 = threadIdx.x + blockIdx.x * blockDim.x;
-  while (x39 < x37) {
-    int x40 = x39;
-    float x41 = x36[x40] * 0.5 + x35[x40];
-    x34[x40] = x34[x40] - x41 * 1.0E-4;
-    x36[x40] = x41;
-    x39 = x39 + x38;
+  int x32 = gridDim.x * blockDim.x;
+  int x33 = threadIdx.x + blockIdx.x * blockDim.x;
+  while (x33 < x31) {
+    int x34 = x33;
+    float x35 = x30[x34] * 0.5 + x29[x34];
+    x28[x34] = x28[x34] - x35 * 1.0E-4;
+    x30[x34] = x35;
+    x33 = x33 + x32;
   }
   // end generating kernel function for SGD of type Float
 }
@@ -102,68 +94,88 @@ void Snippet(int x0) {
   // end initializing fixed GPU array of size 18 and type Float and device (pre-rename) x39
   int x19 = 0;
   while (x19 != 10) {
-    // begin initializing fixed GPU array of size 18 and type Float and device (pre-rename) x39
+    // begin creating and setting tensor descriptor of shape List(2, 1, 3, 3)
+    cudnnTensorDescriptor_t x36;
+    CUDNNCHECK(cudnnCreateTensorDescriptor(&x36));
+    CUDNNCHECK(cudnnSetTensor4dDescriptor(x36, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, 2, 1, 3, 3));
+    // end creating and setting tensor descriptor
+    // begin finding dropout forward reserve bytes
+    size_t x37 = (size_t)0;
+    CUDNNCHECK(cudnnDropoutGetReserveSpaceSize(x36, &x37));
+    // end finding dropout forward reserve bytes
+    // begin finding dropout forward states bytes
+    size_t x38 = (size_t)0;
+    CUDNNCHECK(cudnnDropoutGetStatesSize(x7, &x38));
+    // end finding dropout forward states bytes
+    // begin allocating gpu array for the reserve space of dropout forward
+    CUDA_CALL(cudaSetDevice(x6));
+    float* x39 = (float*)malloc(0 * sizeof(float));
+    CUDA_CALL(cudaMalloc(&x39, (size_t)x37));
+    // end allocating gpu array for the reserve space of dropout forward
+    // begin allocating gpu array for the states of dropout forward
+    CUDA_CALL(cudaSetDevice(x6));
+    float* x40 = (float*)malloc(0 * sizeof(float));
+    CUDA_CALL(cudaMalloc(&x40, (size_t)x38));
+    // end allocating gpu array for the states of dropout forward
+    // begin creating dropout descriptor
+    cudnnDropoutDescriptor_t x41;
+    CUDNNCHECK(cudnnCreateDropoutDescriptor(&x41));
+    CUDNNCHECK(cudnnSetDropoutDescriptor(x41, x7, 0.5, x40, x38, 1));
+    // end creating dropout descriptor
+    // begin allocating gpu array for the output of dropout
     CUDA_CALL(cudaSetDevice(x6));
     float* x42 = (float*)malloc(0 * sizeof(float));
     CUDA_CALL(cudaMalloc(&x42, (size_t)(18 * sizeof(float))));
-    x12<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x42, 0, 18);
+    // end allocating gpu array for the output of dropout
+    // begin dropout forward pass
+    CUDNNCHECK(cudnnDropoutForward(x7, x41, x36, x10, x36, x42, x39, x37));
+    // end dropout forward pass
+    tuple-view(x42, x40, x39);
+    // begin initializing fixed GPU array of size 18 and type Float and device (pre-rename) x39
+    CUDA_CALL(cudaSetDevice(x6));
+    float* x43 = (float*)malloc(0 * sizeof(float));
+    CUDA_CALL(cudaMalloc(&x43, (size_t)(18 * sizeof(float))));
+    x12<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x43, 0, 18);
     // end initializing fixed GPU array of size 18 and type Float and device (pre-rename) x39
-    // begin initializing fixed GPU array of size 18 and type Boolean and device (pre-rename) x39
-    CUDA_CALL(cudaSetDevice(x6));
-    bool* x43 = (bool*)malloc(0 * sizeof(bool));
-    CUDA_CALL(cudaMalloc(&x43, (size_t)(18 * sizeof(bool))));
-    x20<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x43, 0, 18);
-    // end initializing fixed GPU array of size 18 and type Boolean and device (pre-rename) x39
-    // begin initializing fixed GPU array of size 18 and type Boolean and device (pre-rename) x39
-    CUDA_CALL(cudaSetDevice(x6));
-    bool* x44 = (bool*)malloc(0 * sizeof(bool));
-    CUDA_CALL(cudaMalloc(&x44, (size_t)(18 * sizeof(bool))));
-    x20<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x44, 0, 18);
-    // end initializing fixed GPU array of size 18 and type Boolean and device (pre-rename) x39
-    // begin creating and setting tensor descriptor of shape List(2, 1, 3, 3)
-    cudnnTensorDescriptor_t x45;
-    CUDNNCHECK(cudnnCreateTensorDescriptor(&x45));
-    CUDNNCHECK(cudnnSetTensor4dDescriptor(x45, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, 2, 1, 3, 3));
-    // end creating and setting tensor descriptor
     // begin allocating gpu array for the gradient of input of dropout
     CUDA_CALL(cudaSetDevice(x6));
-    float* x46 = (float*)malloc(0 * sizeof(float));
-    CUDA_CALL(cudaMalloc(&x46, (size_t)(18 * sizeof(float))));
+    float* x44 = (float*)malloc(0 * sizeof(float));
+    CUDA_CALL(cudaMalloc(&x44, (size_t)(18 * sizeof(float))));
     // end allocating gpu array for the gradient of input of dropout
     // begin finding dropout backward reserve bytes
-    size_t x47 = (size_t)0;
-    CUDNNCHECK(cudnnDropoutGetReserveSpaceSize(x45, &x47));
+    size_t x45 = (size_t)0;
+    CUDNNCHECK(cudnnDropoutGetReserveSpaceSize(x36, &x45));
     // end finding dropout backward reserve bytes
     // begin finding dropout backward states bytes
-    size_t x48 = (size_t)0;
-    CUDNNCHECK(cudnnDropoutGetStatesSize(x7, &x48));
+    size_t x46 = (size_t)0;
+    CUDNNCHECK(cudnnDropoutGetStatesSize(x7, &x46));
     // end finding dropout backward states bytes
     // begin creating dropout descriptor
-    cudnnDropoutDescriptor_t x49;
-    CUDNNCHECK(cudnnCreateDropoutDescriptor(&x49));
-    CUDNNCHECK(cudnnSetDropoutDescriptor(x49, x7, 0.5, x43, x48, 1));
+    cudnnDropoutDescriptor_t x47;
+    CUDNNCHECK(cudnnCreateDropoutDescriptor(&x47));
+    CUDNNCHECK(cudnnSetDropoutDescriptor(x47, x7, 0.5, x40, x46, 1));
     // end creating dropout descriptor
     // begin dropout backward pass
-    CUDNNCHECK(cudnnDropoutBackward(x7, x49, x45, x42, x45, x46, x44, x47));
+    CUDNNCHECK(cudnnDropoutBackward(x7, x47, x36, x43, x36, x44, x39, x45));
     // end dropout backward pass
-    // begin computing ACCUM on GPU for size 18 and type Float at device (pre-rename) x39 with base_operand x93 and addition_operand x222
+    // begin computing ACCUM on GPU for size 18 and type Float at device (pre-rename) x39 with base_operand x93 and addition_operand x223
     CUDA_CALL(cudaSetDevice(x6));
-    x26<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x11, x46, 18);
-    // end computing ACCUM on GPU for size 18 and type Float at device (pre-rename) x39 with base_operand x93 and addition_operand x222
+    x20<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x11, x44, 18);
+    // end computing ACCUM on GPU for size 18 and type Float at device (pre-rename) x39 with base_operand x93 and addition_operand x223
     // begin computing SGD on GPU for size 18 and type Float at device (pre-name) x39 with weight x70, grad x93, and momentum x131
     CUDA_CALL(cudaSetDevice(x6));
-    x33<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x10, x11, x18, 18);
+    x27<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x10, x11, x18, 18);
     // end computing SGD on GPU for size 18 and type Float at device (pre-name) x39 with weight x70, grad x93, and momentum x131
     x19 = x19 + 1;
   }
   if (x6 == 0) {
     // begin copying GPU array x70 to CPU and print for size 18 and type Float
-    float* x50 = (float*)malloc(18 * sizeof(float));
-    CUDA_CALL(cudaMemcpy(x50, x10, (size_t)(18 * sizeof(float)), cudaMemcpyDeviceToHost));
-    int x51 = 0;
-    while (x51 != 18) {
-      printf("%f ", x50[x51]);
-      x51 = x51 + 1;
+    float* x48 = (float*)malloc(18 * sizeof(float));
+    CUDA_CALL(cudaMemcpy(x48, x10, (size_t)(18 * sizeof(float)), cudaMemcpyDeviceToHost));
+    int x49 = 0;
+    while (x49 != 18) {
+      printf("%f ", x48[x49]);
+      x49 = x49 + 1;
     }
     printf("\n");
     // end copying GPU array x70 to CPU and print for size 18 and type Float
