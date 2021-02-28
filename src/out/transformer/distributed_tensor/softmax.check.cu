a611af4b23aa1962cba1eb90c02c73bfe4bece5f
Luke Jiang <39086121+luke-jiang@users.noreply.github.com>
2021-02-28 18:06:48 -0500

    update

--- src/out/transformer/distributed_tensor/softmax.check.cu
+++ src/out/transformer/distributed_tensor/softmax.check.cu
@@ -94,41 +94,51 @@ void Snippet(int x0) {
   // end initializing fixed GPU array of size 9 and type Float and device (pre-rename) x39
   int x20 = 0;
   while (x20 != 10) {
-    // begin initializing fixed GPU array of size 9 and type Float and device (pre-rename) x39
-    CUDA_CALL(cudaSetDevice(x7));
-    float* x37 = (float*)malloc(0 * sizeof(float));
-    CUDA_CALL(cudaMalloc(&x37, (size_t)(9 * sizeof(float))));
-    x13<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x37, 0, 9);
-    // end initializing fixed GPU array of size 9 and type Float and device (pre-rename) x39
-    // begin initializing fixed GPU array of size 9 and type Float and device (pre-rename) x39
+    // begin creating and setting tensor descriptor of shape List(2, 1, 3, 3)
+    cudnnTensorDescriptor_t x37;
+    CUDNNCHECK(cudnnCreateTensorDescriptor(&x37));
+    CUDNNCHECK(cudnnSetTensor4dDescriptor(x37, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, 2, 1, 3, 3));
+    // end creating and setting tensor descriptor
+    // begin allocating gpu array for the output of softmax
     CUDA_CALL(cudaSetDevice(x7));
     float* x38 = (float*)malloc(0 * sizeof(float));
-    CUDA_CALL(cudaMalloc(&x38, (size_t)(9 * sizeof(float))));
-    x13<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x38, 1, 9);
-    // end initializing fixed GPU array of size 9 and type Float and device (pre-rename) x39
-    // begin computing ACCUM on GPU for size 9 and type Float at device (pre-rename) x39 with base_operand x142 and addition_operand x155
+    CUDA_CALL(cudaMalloc(&x38, (size_t)(18 * sizeof(float))));
+    // end allocating gpu array for the output of softmax
+    // begin softmax forward pass
+    float x39 = 1.0;
+    float x40 = 0.0;
+    CUDNNCHECK(cudnnSoftmaxForward(x8, CUDNN_SOFTMAX_FAST, CUDNN_SOFTMAX_MODE_INSTANCE, &x39, x37, &x11, &x40, x37, &x38));
+    // end softmax forward pass
+    // begin initializing fixed GPU array of size 9 and type Float and device (pre-rename) x39
     CUDA_CALL(cudaSetDevice(x7));
-    x21<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x37, x38, 9);
-    // end computing ACCUM on GPU for size 9 and type Float at device (pre-rename) x39 with base_operand x142 and addition_operand x155
-    // begin creating and setting tensor descriptor of shape List(2, 1, 3, 3)
-    cudnnTensorDescriptor_t x39;
-    CUDNNCHECK(cudnnCreateTensorDescriptor(&x39));
-    CUDNNCHECK(cudnnSetTensor4dDescriptor(x39, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, 2, 1, 3, 3));
-    // end creating and setting tensor descriptor
+    float* x41 = (float*)malloc(0 * sizeof(float));
+    CUDA_CALL(cudaMalloc(&x41, (size_t)(9 * sizeof(float))));
+    x13<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x41, 0, 9);
+    // end initializing fixed GPU array of size 9 and type Float and device (pre-rename) x39
+    // begin initializing fixed GPU array of size 9 and type Float and device (pre-rename) x39
+    CUDA_CALL(cudaSetDevice(x7));
+    float* x42 = (float*)malloc(0 * sizeof(float));
+    CUDA_CALL(cudaMalloc(&x42, (size_t)(9 * sizeof(float))));
+    x13<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x42, 1, 9);
+    // end initializing fixed GPU array of size 9 and type Float and device (pre-rename) x39
+    // begin computing ACCUM on GPU for size 9 and type Float at device (pre-rename) x39 with base_operand x169 and addition_operand x182
+    CUDA_CALL(cudaSetDevice(x7));
+    x21<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x41, x42, 9);
+    // end computing ACCUM on GPU for size 9 and type Float at device (pre-rename) x39 with base_operand x169 and addition_operand x182
     // begin allocating gpu array for the gradient of input of softmax
     CUDA_CALL(cudaSetDevice(x7));
-    float* x40 = (float*)malloc(0 * sizeof(float));
-    CUDA_CALL(cudaMalloc(&x40, (size_t)(18 * sizeof(float))));
+    float* x43 = (float*)malloc(0 * sizeof(float));
+    CUDA_CALL(cudaMalloc(&x43, (size_t)(18 * sizeof(float))));
     // end allocating gpu array for the gradient of input of softmax
     // begin softmax backward pass
-    float x41 = 1.0;
-    float x42 = 0.0;
-    CUDNNCHECK(cudnnSoftmaxBackward(x8, CUDNN_SOFTMAX_FAST, CUDNN_SOFTMAX_MODE_INSTANCE, &x41, &x39, &x11, x39, &x37, &x42, x39, &x40));
+    float x44 = 1.0;
+    float x45 = 0.0;
+    CUDNNCHECK(cudnnSoftmaxBackward(x8, CUDNN_SOFTMAX_FAST, CUDNN_SOFTMAX_MODE_INSTANCE, &x44, x37, &x38, x37, &x41, &x45, x37, &x43));
     // end softmax backward pass
-    // begin computing ACCUM on GPU for size 9 and type Float at device (pre-rename) x39 with base_operand x86 and addition_operand x214
+    // begin computing ACCUM on GPU for size 9 and type Float at device (pre-rename) x39 with base_operand x86 and addition_operand x232
     CUDA_CALL(cudaSetDevice(x7));
-    x21<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x12, x40, 9);
-    // end computing ACCUM on GPU for size 9 and type Float at device (pre-rename) x39 with base_operand x86 and addition_operand x214
+    x21<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x12, x43, 9);
+    // end computing ACCUM on GPU for size 9 and type Float at device (pre-rename) x39 with base_operand x86 and addition_operand x232
     // begin computing SGD on GPU for size 9 and type Float at device (pre-name) x39 with weight x70, grad x86, and momentum x124
     CUDA_CALL(cudaSetDevice(x7));
     x28<<<dim3(28, 1, 1), dim3(512, 1, 1)>>>(x11, x12, x19, 9);
@@ -136,42 +146,42 @@ void Snippet(int x0) {
     x20 = x20 + 1;
   }
   // Only declare recv buffer if this is the root
-  bool x43 = x7 == 0;
-  float* x44 = x43 ? ({
-    float* x45 = (float*)malloc(0 * sizeof(float));
-    CUDA_CALL(cudaMalloc(&x45, (size_t)(18 * sizeof(float))));
-    x45;
+  bool x46 = x7 == 0;
+  float* x47 = x46 ? ({
+    float* x48 = (float*)malloc(0 * sizeof(float));
+    CUDA_CALL(cudaMalloc(&x48, (size_t)(18 * sizeof(float))));
+    x48;
   }) : ({
-    float* x46 = (float*)malloc(0 * sizeof(float));
-    CUDA_CALL(cudaMalloc(&x46, (size_t)0));
-    x46;
+    float* x49 = (float*)malloc(0 * sizeof(float));
+    CUDA_CALL(cudaMalloc(&x49, (size_t)0));
+    x49;
   });
   // Gather by groups of NCCL send/recv
   NCCLCHECK(ncclGroupStart());
-  ncclResult_t x47 = ncclSend(x11, (size_t)18, ncclFloat32, 0, x5, x6);
-  NCCLCHECK(x47);
-  if (x43) {
-    int x48 = x1;
-    int x49 = 0;
-    while (x49 != x48) {
-      int x50 = x49;
-      NCCLCHECK(ncclRecv(x44 + x50 * 9, (size_t)18, ncclFloat32, x50, x5, x6));
-      x49 = x49 + 1;
+  ncclResult_t x50 = ncclSend(x11, (size_t)18, ncclFloat32, 0, x5, x6);
+  NCCLCHECK(x50);
+  if (x46) {
+    int x51 = x1;
+    int x52 = 0;
+    while (x52 != x51) {
+      int x53 = x52;
+      NCCLCHECK(ncclRecv(x47 + x53 * 9, (size_t)18, ncclFloat32, x53, x5, x6));
+      x52 = x52 + 1;
     }
   }
   NCCLCHECK(ncclGroupEnd());
   // print the array only if this is the root
-  if (x43) {
-    // begin copying GPU array x298 to CPU and print for size 18 and type Float
-    float* x51 = (float*)malloc(18 * sizeof(float));
-    CUDA_CALL(cudaMemcpy(x51, x44, (size_t)(18 * sizeof(float)), cudaMemcpyDeviceToHost));
-    int x52 = 0;
-    while (x52 != 18) {
-      printf("%f ", x51[x52]);
-      x52 = x52 + 1;
+  if (x46) {
+    // begin copying GPU array x316 to CPU and print for size 18 and type Float
+    float* x54 = (float*)malloc(18 * sizeof(float));
+    CUDA_CALL(cudaMemcpy(x54, x47, (size_t)(18 * sizeof(float)), cudaMemcpyDeviceToHost));
+    int x55 = 0;
+    while (x55 != 18) {
+      printf("%f ", x54[x55]);
+      x55 = x55 + 1;
     }
     printf("\n");
-    // end copying GPU array x298 to CPU and print for size 18 and type Float
+    // end copying GPU array x316 to CPU and print for size 18 and type Float
   }
   printf("compile");
   CUDNNCHECK(cudnnDestroy(x8));
