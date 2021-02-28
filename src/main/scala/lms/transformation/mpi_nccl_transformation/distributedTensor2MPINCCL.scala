db4739cbcc8124e38e95966dab2541d6f9278392
Luke Jiang <39086121+luke-jiang@users.noreply.github.com>
2021-02-28 13:59:18 -0500

    change typo

--- src/main/scala/lms/transformation/mpi_nccl_transformation/distributedTensor2MPINCCL.scala
+++ src/main/scala/lms/transformation/mpi_nccl_transformation/distributedTensor2MPINCCL.scala
@@ -33,7 +33,7 @@ class DistributeTensor2MPI_NCCLAnalysis extends Traverser {
         case Node(s, op, _, _) if op.startsWith("tensor_conv") =>
             hasCudnn = true
             super.traverse(n)
-        case Node(s, op, _, _) if op.startsWith("tensor_dropout") =>
+        case Node(s, op, _, _) if op.startsWith("tensors_dropout") =>
             hasCudnn = true
             super.traverse(n)
         case _ => super.traverse(n)
