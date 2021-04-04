3d6d2dfd0495b1b59866339b0c0d6d1bda27cd19
Luke Jiang <39086121+luke-jiang@users.noreply.github.com>
2021-04-04 14:49:58 -0400

    add maskedFill backend implementation

--- src/main/scala/lms/thirdparty/cuda/cuda.scala
+++ src/main/scala/lms/thirdparty/cuda/cuda.scala
@@ -913,6 +913,35 @@ trait CudaLibs extends CudaOps {
     }
   }
 
+  def maskedFillGrad[N:Numeric:Manifest](ijSwapped: Boolean)(implicit __pos: SourceContext) = cudaGlobalFun {
+    (y_d: Rep[Array[N]], x_d: Rep[Array[N]], mask: Rep[Array[Int]],
+    dim0_shape: Rep[Int], dim1_shape: Rep[Int], dim0_stride: Rep[Int], dim1_stride: Rep[Int],
+    input_size: Rep[Int]) => {
+
+      val tid = var_new[Int](blockIdxX * blockDimX + threadIdxX)
+      val stride = blockDimX * gridDimX
+
+      val i = var_new[Int](tid / dim0_stride)
+      val j = var_new[Int]((tid - i * dim0_stride) / dim1_stride)
+      val inner_idx = var_new[Int](tid - i * dim0_stride - j * dim1_stride)
+      val idx = var_new[Int](i * dim0_stride + j * dim1_stride + inner_idx)
+
+      __whileDo(idx < input_size, {
+        val mask_id = if (ijSwapped)
+          (j % dim1_shape) * dim0_shape + (i % dim0_shape) else
+          (i % dim0_shape) * dim1_shape + (j % dim1_shape)
+
+        __ifThenElse(ordering_equiv(mask(mask_id), 0), { x_d(idx) = x_d(idx) + y_d(idx) }, {  })
+
+        __assign(tid, tid + stride)
+        __assign(i, tid / dim0_stride)
+        __assign(j, (tid - i * dim0_stride) / dim1_stride)
+        __assign(inner_idx, tid - i * dim0_stride - j * dim1_stride)
+        __assign(idx, i * dim0_stride + j * dim1_stride + inner_idx)
+      })
+    }
+  }
+
   // Cuda Tranpose Values
   val blockRows = 8
   val tileDim = 32
