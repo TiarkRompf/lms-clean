fix permute shape computation

src/test/scala/lms/transformation/test_distributed_tensor/permute_102_big.py
@@ -9,16 +9,16 @@ def generate_data(lms_clean_root: str):
     torch.manual_seed(0)
 
     # model
-    dimX = 600
-    dimY = 60
-    dimZ = 80
+    dimX = 8
+    dimY = 2
+    dimZ = 8
 
     input = torch.randn(dimZ, dimY, dimX)
     input.requires_grad = True
     loss = input.permute(1, 0, 2)
     loss.sum().backward()
 
-    printer = get_printer(lms_clean_root, test_name=f"permute3D_{permute[0]}{permute[1]}{permute[2]}")
+    printer = get_printer(lms_clean_root, test_name="permute_102_big")
     printer("input", input, dim=0, degree=2)
     printer("loss", loss, dim=1, degree=2)
     printer("input_grad", input.grad, dim=0, degree=2)