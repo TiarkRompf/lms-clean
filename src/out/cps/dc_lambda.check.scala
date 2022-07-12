a8ef79532a7b79a99bda8a2ea61f532425f218e7
Guannan Wei <guannanwei@outlook.com>
2022-07-12 14:30:06 -0400

    fix test files (variable naming diff due to backend change)

--- src/out/cps/dc_lambda.check.scala
+++ src/out/cps/dc_lambda.check.scala
@@ -5,21 +5,21 @@ class Snippet extends (Int => Int) {
       val x9 = x8 * 2
       c(x9)
     }
-    def cApp0(x15: Int) = {
-      def cApp1(x16: Int) = {
-        val x18 = {
+    def cApp0(x16: Int) = {
+      def cApp1(x17: Int) = {
+        val x19 = {
           def x11(x14: Int) = {
-            val x17 = x14 + x16
-            x17
+            val x18 = x14 + x17
+            x18
           }
           val x12 = x11(x1)
           val x13 = x11(x12)
           x13
         }
-        val x19 = x18 + 5
-        x19 /*exit x19*/
+        val x20 = x19 + 5
+        x20 /*exit x20*/
       }
-      x6(cApp1, x15)
+      x6(cApp1, x16)
     }
     x6(cApp0, x3)
   }
--- src/out/cps/whileDc.check.scala
+++ src/out/cps/whileDc.check.scala
@@ -1,16 +1,16 @@
 class Snippet extends (Int => Int) {
   def apply(x1: Int): Int = {
-    val x26 = {
+    val x27 = {
       var x3 = 0
       var x4 = 0
       def loop0(): Int = {
         val x6 = x3
         val x7 = x6 < 5
         if (x7) {
-          def x10(x19: Int) = {
-            val x21 = x3
-            val x22 = x21 + 1
-            x3 = x22
+          def x10(x20: Int) = {
+            val x22 = x3
+            val x23 = x22 + 1
+            x3 = x23
             loop0()
           }
           val x11 = x4
@@ -23,14 +23,14 @@ class Snippet extends (Int => Int) {
           x4 = x17
           x17
         } else {
-          val x25 = x4
-          x25
+          val x26 = x4
+          x26
         }
       }
       loop0()
     }
-    val x27 = x26 + 4
-    x27 /*exit x27*/
+    val x28 = x27 + 4
+    x28 /*exit x28*/
   }
 }
 // output:
