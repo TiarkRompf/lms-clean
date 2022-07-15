1991c875cd59efe03be9204d1e6eae002dff743e
Guannan Wei <guannanwei@outlook.com>
2022-07-15 15:56:34 -0700

    space

--- src/out/backend/fac-01.check.scala
+++ src/out/backend/fac-01.check.scala
@@ -1,11 +1,11 @@
 // Raw:
 x5 = (!= x4 0)
 x7 = (- x4 1)
-x8 = (@ x2 x7)  [CTRL*: _ | x6]
+x8 = (@ x2 x7) [CTRL*: _ | x6]
 x9 = (* x4 x8)
-x11 = (? x5 Block(List(),x9,x6,[CTRL*: _ | x8]) Block(List(),1,x10,[: _ | x10]))  [CTRL*: _ | x3]
+x11 = (? x5 Block(List(),x9,x6,[CTRL*: _ | x8]) Block(List(),1,x10,[: _ | x10])) [CTRL*: _ | x3]
 x2 = (λ Block(List(x4),x11,x3,[CTRL*: _ | x11]))
-x12 = (@ x2 x1)  [CTRL*: _ | x0]
+x12 = (@ x2 x1) [CTRL*: _ | x0]
 // Generic Codegen:
 // in: x1 effect: x0
 x2 = (λ {
